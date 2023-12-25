21307744afa445f6995c810b59f3d7e5f1806327
whatziya <dilshodqudratov522@gmail.com>
2023-12-25 19:23:40 +0500

    try harder

--- src/lesson_1/vazifa_5.java
+++ src/lesson_1/vazifa_5.java
@@ -4,16 +4,6 @@ import java.util.Scanner;
 
 public class vazifa_5 {
     public static void main(String[] args){
-        String a,b;
-        Scanner scanner = new Scanner(System.in);
-        a = scanner.nextLine();
-        b = scanner.nextLine();
-        char[] aChar = a.toCharArray();
-        char[] bChar = b.toCharArray();
-        int res = bChar.length;
-        for(int i = 0; i < aChar.length; i++){
-
-        }
         System.out.println("Ha");
     }
 }
